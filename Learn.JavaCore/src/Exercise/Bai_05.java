c7fec12006d56a1304dc385f6157029647d5ce7f
phandinh2010 <phandinh2010@gmail.com>
2021-05-14 23:41:08 +0700

    lam bai buoi 5

---
+++ Learn.JavaCore/src/Exercise/Bai_05.java
@@ -0,0 +1,37 @@
+package Exercise;
+
+import java.util.Scanner;
+
+public class Bai_05 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Hãy nhập số n:");
+		int n = sc.nextInt();
+
+		Show_Prime(n);
+
+	}
+
+	public static void Show_Prime(int n) {
+		for (int i = 1; i < n; i++) {
+			if (isPrime(i)) {
+				System.out.println(i + " ");
+			}
+		}
+	}
+
+	public static boolean isPrime(int n) {
+		// boolean result;
+
+		if (n <= 2) {
+			return true;
+		} else {
+			for (int i = 2; i <= Math.sqrt(n); i++)
+				if (n % i == 0) {
+					return false;
+				}
+		}
+		return true;
+	}
+}
