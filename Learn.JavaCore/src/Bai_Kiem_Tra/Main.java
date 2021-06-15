73693e74e4b88f09dd145c64fab772471256c7aa
dinhpt5 <dinhpt5@VID-DINHPT5-L.mshome.net>
2021-06-15 13:38:29 +0700

    Update main

--- Learn.JavaCore/src/Bai_Kiem_Tra/Main.java
+++ Learn.JavaCore/src/Bai_Kiem_Tra/Main.java
@@ -5,9 +5,9 @@ public class Main {
 	
 
 	public static void main(String[] args) {
-//		String str = "You Only Live Once. But if You do it right. once is Enough";
-//		Bai_01 bai_01 = new Bai_01();
-//		System.out.println(bai_01.Count_Lowercase(str));
+		String str = "You Only Live Once. But if You do it right. once is Enough";
+		Bai_01 bai_01 = new Bai_01();
+		System.out.println(bai_01.Count_Lowercase(str));
 		
 		Bai_02 bai_02 = new Bai_02();		
 		bai_02.CheckPassword();
